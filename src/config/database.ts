10cd2ef435a4b9b8802651274cd176290739d11f
Tejaswa <mathurkiit@gmail.com>
2025-05-30 04:34:41 +0530

    chore: prepare app for deployment on Render

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,17 +2,35 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
+let sequelize: Sequelize;
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        port: Number(process.env.DB_PORT),
-        logging: false,
-    }
-);
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME as string,
+        process.env.DB_USER as string,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'postgres',
+            port: Number(process.env.DB_PORT),
+            logging: false,
+        }
+    );
+}
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME as string,
+//     process.env.DB_USER as string,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: process.env.DB_HOST,
+//         dialect: 'postgres',
+//         port: Number(process.env.DB_PORT),
+//         logging: false,
+//     }
+// );
 
 export default sequelize;
+
+
